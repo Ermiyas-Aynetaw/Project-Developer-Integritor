3df69700769dc3a991a4fdc1e226339e2057313f
Ermiyas-Aynetaw <87368573+Ermiyas-Aynetaw@users.noreply.github.com>
2023-09-02 12:04:46 +0300

    Update settings.py

--- myprojects/settings.py
+++ myprojects/settings.py
@@ -173,7 +173,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = 'ermitheethio16@gmail.com'
+EMAIL_HOST_USER = 'eromitempo@gmail.com'
 EMAIL_HOST_PASSWORD  = 'usycigtpuxbrrpux'
 
 # Static files (CSS, JavaScript, Images)
